b1ad098efbc6836313c76fda55fed877f674b318
williambpereira21 <william.pbarbosagon@gmail.com>
2025-08-18 18:50:23 -0300

    Descrição das mudanças feitas

--- aula04.1/03.py
+++ aula04.1/03.py
@@ -1,17 +1,17 @@
-print('digite sua idade:')
-idade= int(input())
+print('Digite sua idade:')
+idade = int(input())
 
-if idade<18:
-    print('você não é maior de idade!')
-    print('não poderá realizar operações bancárias!')
-    print('obrigado por escolher os nossos serviços!')
+if idade < 18:
+    print('Você não é maior de idade!')
+    print('Não poderá realizar operações bancárias!')
+    print('Obrigado por escolher os nossos serviços!')
 
-elif idade>=65:
-    print('você está aposentado')
-print('poderemos oferecer suporte técnico...')
+elif idade >= 65:
+    print('Você está aposentado')
+    print('Poderemos oferecer suporte técnico...')
 
+else:
+    print('Você é maior de idade.')
+    print('Portanto, pode realizar transações bancárias.')
+    print('Obrigado por escolher os nossos serviços!')
 
-else idade<18:
-print(' você é maior de idade.')
-print('portanto,pode realizar transações bancárias')
-print('obrigado por escolher os nossos serviços!')
