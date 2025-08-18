7cd60150a99f338f4b9d402976b2c3680f9f11e0
williambpereira21 <william.pbarbosagon@gmail.com>
2025-08-18 18:51:08 -0300

    Descrição das mudanças feitas

--- aula04.1/04.py
+++ aula04.1/04.py
@@ -1,21 +1,21 @@
-#demonstração do uso de condicional match/case....
-print('digite o número referente ao estado da moeda:')
+# demonstração do uso de condicional match/case....
+print('Digite o número referente ao estado da moeda:')
 print('1. flor de cunho')
 print('2. soberba')
-print('3.muito bem conservada')
-print('5. outros estados')
-estado=int(input())
+print('3. muito bem conservada')
+print('4. outros estados')
+estado = int(input())
 
 match estado:
     case 1:
-        print('perfeita! vou pagar 1.000.000,00!')
-        case 2:
-        print('quase perfeita! vou pagar 250.000,00!')
-        case 3:
-        print('que ótimo! vou pagar 30.000,00!')
-        case 4:
-        print('desculpe não posso aceitar nesse estado')
-        case 5:
-        print('desculpe não posso aceitar nesse estado')
-        case _:
-        print('desculpe opção não reconhecida')
+        print('Perfeita! Vou pagar 1.000.000,00!')
+    case 2:
+        print('Quase perfeita! Vou pagar 250.000,00!')
+    case 3:
+        print('Que ótimo! Vou pagar 30.000,00!')
+    case 4:
+        print('Desculpe, não posso aceitar nesse estado')
+    case 5:
+        print('Desculpe, não posso aceitar nesse estado')
+    case _:
+        print('Desculpe, opção não reconhecida')
